cb64f9356f21ac9ba925fc03831c9d87b061072b
JuanquiZabalo <jucazato95@gmail.com>
2024-03-26 21:27:35 +0100

    Crear componente Applayout

---
+++ src/components/ui/layouts/AppLayouts.jsx
@@ -0,0 +1,19 @@
+import Footer from "../../Footer"
+import Header from "../../Header"
+import PropTypes from 'prop-types'
+
+export default function AppLayout({children}){
+    return(
+        <>
+        <Header/>
+        <main className="min-h-[85vh]">{children}</main>
+        <Footer/>
+        </>
+    )
+}
+
+AppLayout.prototype = {
+    children: PropTypes.node.isRequired
+}
+
+
