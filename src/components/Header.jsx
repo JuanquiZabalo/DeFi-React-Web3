ddbd8d62702c443b19ff05d4ee2e2803a2811897
JuanquiZabalo <jucazato95@gmail.com>
2024-03-26 21:27:19 +0100

    Crear componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header(){
+    return(
+        <header className="bg-white border-b shadow flex justify-between items-center">
+            <img src="/logo-mobile-blockmaker.ico" alt="logo de blockmaker mobile" className="sm:hidden" width={46}/>
+            <img src="/logo-full-blockmaker.png" alt="logo de blockmaker desktop" className="hidden sm:flex" width={300}/>
+            <button className="bg-gray-400 rounded-xl text-sm px-2 py-1 h-fit">Connect Wallet</button>
+        </header>
+    )
+}
