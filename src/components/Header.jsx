71ca252844fcd04b9e3fe6a0d883642eb9a403c7
JuanquiZabalo <jucazato95@gmail.com>
2024-03-27 16:13:00 +0100

    Anadir boton connect wallet

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { ConnectKitButton } from "connectkit";
+
 export default function Header(){
     return(
         <header className="bg-white border-b shadow flex justify-between items-center">
             <img src="/logo-mobile-blockmaker.ico" alt="logo de blockmaker mobile" className="sm:hidden" width={46}/>
             <img src="/logo-full-blockmaker.png" alt="logo de blockmaker desktop" className="hidden sm:flex" width={300}/>
-            <button className="bg-gray-400 rounded-xl text-sm px-2 py-1 h-fit">Connect Wallet</button>
+            <ConnectKitButton showBalance={true}/>
+            {/*<button className="bg-gray-400 rounded-xl text-sm px-2 py-1 h-fit">Connect Wallet</button>*/}
         </header>
     )
 }
