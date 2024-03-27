f68bed0123abe5e13b67ecd82873b60322fd6119
JuanquiZabalo <jucazato95@gmail.com>
2024-03-27 16:11:52 +0100

    Anadir componente Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import { useAccount } from "wagmi"
+
+export default function Home(){
+    const { address, isConnecting, isDisconnected } = useAccount();
+    if (isConnecting) return <div>Connecting...</div>;
+    if (isDisconnected) return <div>Disconnected</div>;
+    
+    return <div>Connected Wallet: {address}</div>;
+   
+}
