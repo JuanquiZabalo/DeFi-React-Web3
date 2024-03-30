6ebabc1ba382b2a9f5c54854939d027e39449509
JuanquiZabalo <jucazato95@gmail.com>
2024-03-30 20:57:18 +0100

    Actualizar componente Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,41 @@
-import { useAccount } from "wagmi"
+import { useAccount } from "wagmi";
+import {
+  LoadingSpinner,
+  Button,
+  TextInput,
+  Title,
+} from "../components/ui";
+import { Nav, Administrador } from "../components";
 
-export default function Home(){
-    const { address, isConnecting, isDisconnected } = useAccount();
-    if (isConnecting) return <div>Connecting...</div>;
-    if (isDisconnected) return <div>Disconnected</div>;
-    
-    return <div>Connected Wallet: {address}</div>;
-   
+export default function Home() {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  if (isConnecting) return <div>Connecting...</div>;
+  if (isDisconnected) return <div>Disconnected</div>;
+
+  return (
+    <>
+      <div className="flex justify-center p-3">
+        <Nav />
+      </div>
+      <div className="flex flex-col gap-2">
+        <div>Connected Wallet: {address}</div>
+        
+        <LoadingSpinner />
+
+        <div>
+          <Button disable={false}>Click me</Button>
+        </div>
+        
+
+        <div>
+          <TextInput />
+        </div>
+
+        <Title>Titulo de prueba</Title>
+
+        <Administrador/>
+
+      </div>
+    </>
+  );
 }
