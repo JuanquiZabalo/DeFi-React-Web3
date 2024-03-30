838a74cfbd373ebfabed2123863a3889e7c2272a
JuanquiZabalo <jucazato95@gmail.com>
2024-03-30 20:50:15 +0100

    Agregar componente pagina Usuarios

---
+++ src/pages/GestionUsuarios.jsx
@@ -0,0 +1,12 @@
+import { Nav } from "../components";
+
+export default function GestionUsuarios() {
+  return (
+    <div>
+      <div className="flex justify-center p-3">
+        <Nav />
+      </div>
+      <h1>Pagina gestion de usuarios</h1>
+    </div>
+  );
+}
