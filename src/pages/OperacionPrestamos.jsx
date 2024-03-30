6a55af1f99356ac06c95327d5f34c6b2c44a25d6
JuanquiZabalo <jucazato95@gmail.com>
2024-03-30 20:49:46 +0100

    Agregar componente pagina Operacion Prestamo

---
+++ src/pages/OperacionPrestamos.jsx
@@ -0,0 +1,12 @@
+import { Nav } from "../components";
+
+export default function OperacionPrestamos() {
+  return (
+    <div>
+      <div className="flex justify-center p-3">
+        <Nav />
+      </div>
+      <h1>Pagina Operacion Prestamos</h1>
+    </div>
+  );
+}
