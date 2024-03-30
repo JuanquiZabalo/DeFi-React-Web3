c4872d483fc69e1122ce9ed0d5e0377d4bd54af8
JuanquiZabalo <jucazato95@gmail.com>
2024-03-30 20:51:19 +0100

    Agregar export de paginas a archivo indice: - Gestion usuarios -Operacio Prestamos -Not found

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,4 @@
 export {default as Home} from './Home'
+export {default as GestionUsuarios} from './GestionUsuarios'
+export {default as OperacionPrestamos} from './OperacionPrestamos'
+export {default as NotFound} from './NotFound'
