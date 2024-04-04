2827d4ee4f70b27a0159b5a41f8a782a1ec67529
JuanquiZabalo <jucazato95@gmail.com>
2024-04-04 19:53:28 +0200

    agregar nueva pagina de usuariosprestamistas y usuarios clientes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 export {default as Home} from './Home'
-export {default as GestionUsuarios} from './GestionUsuarios'
+export {default as UsuariosPrestamistas} from './UsuariosPrestamistas'
+export {default as UsuariosClientes} from './UsuariosClientes'
 export {default as OperacionPrestamos} from './OperacionPrestamos'
 export {default as NotFound} from './NotFound'
