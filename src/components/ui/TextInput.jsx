2be604a841ef0d94a750d83941dbcdd40505eb70
JuanquiZabalo <jucazato95@gmail.com>
2024-04-04 19:52:10 +0200

    actualizar estilos

--- src/components/ui/TextInput.jsx
+++ src/components/ui/TextInput.jsx
@@ -1,17 +1,16 @@
 import PropTypes from 'prop-types'
 
-export default function TextInput({type='text', value, onChange, placeholder='To', disabled}){
+export default function TextInput({type='text', value, onChange, placeholder, disabled}){
     return(
         <input 
-        title="To" 
         type={type} 
         value={value} 
         onChange={onChange}
         placeholder={placeholder} 
         disabled= {disabled}
-        className="border border-gray-300 rounded-lg p-2 outline-none
+        className="border border-gray-100 rounded-lg p-2 outline-none
         focus:ring-2 focus:ring-blue-500 focus:border-transparent
-        transition duration-300 ease-in-out disabled:cursor-not-allowed" />
+        transition duration-300 ease-in-out disabled:cursor-not-allowed sm:w-[450px] w-[375px]" />
     )
 }
 
