5598d8fd5d17c9dfbd91eceac592d8e89a535b0c
JuanquiZabalo <jucazato95@gmail.com>
2024-04-04 19:52:23 +0200

    agregar prop de estilo

--- src/components/ui/Title.jsx
+++ src/components/ui/Title.jsx
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types'
 
-export default function Title({children}){
+export default function Title({children, classname}){
     return(
-        <h1 className='text-lg font-bold w-fit'>{children}</h1>
+        <h1 className={`text-lg font-bold ${classname} sm:w-[450px] w-[375px]`}>{children}</h1>
     )
 }
 
 Title.propTypes = {
-    children: PropTypes.node.isRequired
+    children: PropTypes.node.isRequired,
+    classname: PropTypes.string
 }
