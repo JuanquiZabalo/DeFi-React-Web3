47521b2bb687add71815ac1d8bdfd344ddc2ab59
JuanquiZabalo <jucazato95@gmail.com>
2024-04-04 19:46:04 +0200

    actualizar paginas

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { WagmiConfig } from "wagmi";
 import { ConnectKitProvider } from "connectkit";
 import { AppLayout } from "./components/ui/layouts";
 import configWagmi from "./config/wagmi";
-import { Home, GestionUsuarios, OperacionPrestamos, NotFound } from "./pages";
+import { Home, UsuariosPrestamistas, UsuariosClientes, OperacionPrestamos, NotFound } from "./pages";
 import { Route, Routes } from "react-router-dom";
 
 console.log("Alchemy ID:", import.meta.env.VITE_ALCHEMY_ID);
@@ -15,7 +15,8 @@ function App() {
           <AppLayout>
             <Routes>
               <Route index element={<Home />} />
-              <Route path="usuarios" element={<GestionUsuarios />} />
+              <Route path="usuariosPrestamistas" element={<UsuariosPrestamistas />} />
+              <Route path="usuariosClientes" element={<UsuariosClientes />} />
               <Route path="prestamos" element={<OperacionPrestamos />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
